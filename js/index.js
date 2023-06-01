0d2429540d7f8138674af492148638f7915a7247
Sdager <sdagerok@gmail.com>
2023-06-01 17:26:41 +0300

    First commit

---
+++ js/index.js
@@ -0,0 +1,13 @@
+function fun1(){
+    var check;
+    check = document.getElementById('happy');
+
+    if (check.checked){
+        alert('Вы согласились с пользоватедбским соглашением!');
+
+
+    }
+    else {
+        alert('Вы не поставили глаочку у пользовательского соглашения!')
+    }
+}
